a722dfd5e0e91e99638e52add1304635b95a22bd
oleksandrkravcuk <134079899+oleksandrkravcuk@users.noreply.github.com>
2024-04-22 11:54:49 +0000

    Deploying to gh-pages from @ oleksandrkravcuk/Campers_rent@2fe602c6c5e02d4acce507be028a4fab6affc185 🚀

---
+++ static/js/629.0e0dfb2d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkCampers_rent=self.webpackChunkCampers_rent||[]).push([[629],{629:(t,e,L)=>{L.d(e,{mH7:()=>a});var r=L(2119);function a(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",strokeWidth:"2",d:"M5,6 L1,4.5 L1,18.443038 L12,23 L23,18.443038 L23,4 L19,6 M5,16 L5,2 L12,5 L19,2 L19,16 L12,19 L5,16 Z M11.95,5 L11.95,19"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=629.0e0dfb2d.chunk.js.map
